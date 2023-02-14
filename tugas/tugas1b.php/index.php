e34e65d119507a273a02ae902dde8466d65afc47
rmdhniki <rmdhniki18@gmail.com>
2023-02-14 22:09:13 +0700

    first commit

---
+++ tugas/tugas1b.php/index.php
@@ -0,0 +1,34 @@
+<?php 
+    $npm = 46 ;
+    $perkalian = 5 * $npm;
+    $pembagian =  $perkalian / 2;
+    $penambahan = 75 + $pembagian;
+    $pengurangan = $pembagian - 20;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 2</title>
+</head>
+<body>
+    <p>Aku adalah angka <b><?php echo $npm ?></b><br>
+    Jika aku dikali 5, maka aku sekarang <b><?php echo $perkalian ?></b><br>
+    Jika aku dibagi 2, maka aku sekarang <b><?php echo $pembagian ?></b><br>
+    Jika aku ditambah 75, maka aku sekarang <b><?php echo $penambahan ?></b><br>
+    Jika aku dikurang 20, maka aku sekarang <b><?php echo $pengurangan ?></b><br>
+
+
+
+
+    
+</p>
+        
+
+
+
+</body>
+</html>
